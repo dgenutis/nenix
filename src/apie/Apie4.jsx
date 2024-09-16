45744f83ed9ced96a6ac3281606b3ae6058d1a9e
dgenutis <81755948+dgenutis@users.noreply.github.com>
2024-09-16 12:34:51 +0300

    Update Apie4.jsx

--- src/apie/Apie4.jsx
+++ src/apie/Apie4.jsx
@@ -6,48 +6,48 @@ const Apie4 = () => {
         <h2>Mūsų komanda</h2>
         <div className="musuNariai">
           <div className="narys1">
-            <img src="../../public/komanda1.png" alt="komanda1" />
+            <img src="/komanda1.png" alt="komanda1" />
             <div className="ikoneles">
               <a>
-                <img src="../../public/fb.svg" alt="fb" />
+                <img src="/fb.svg" alt="fb" />
               </a>
               <a>
-                <img src="../../public/insta.svg" alt="insta" />
+                <img src="/insta.svg" alt="insta" />
               </a>
               <a>
-                <img src="../../public/linkedin.svg" alt="linkedin" />
+                <img src="/linkedin.svg" alt="linkedin" />
               </a>
             </div>
             <h3>Eglė Petrauskaitė</h3>
             <p>Įkūrėja ir generalinė direktorė</p>
           </div>
           <div className="narys2">
-            <img src="../../public/komanda2.png" alt="komanda2" />
+            <img src="/komanda2.png" alt="komanda2" />
             <div className="ikoneles">
               <a>
-                <img src="../../public/fb.svg" alt="fb" />
+                <img src="/fb.svg" alt="fb" />
               </a>
               <a>
-                <img src="../../public/insta.svg" alt="insta" />
+                <img src="/insta.svg" alt="insta" />
               </a>
               <a>
-                <img src="../../public/linkedin.svg" alt="linkedin" />
+                <img src="/linkedin.svg" alt="linkedin" />
               </a>
             </div>
             <h3>Mantas Vaitkus</h3>
             <p>Rinkodaros vadovas</p>
           </div>
           <div className="narys3">
-            <img src="../../public/komanda3.png" alt="komanda3" />
+            <img src="/komanda3.png" alt="komanda3" />
             <div className="ikoneles">
               <a>
-                <img src="../../public/fb.svg" alt="fb" />
+                <img src="/fb.svg" alt="fb" />
               </a>
               <a>
-                <img src="../../public/insta.svg" alt="insta" />
+                <img src="/insta.svg" alt="insta" />
               </a>
               <a>
-                <img src="../../public/linkedin.svg" alt="linkedin" />
+                <img src="/linkedin.svg" alt="linkedin" />
               </a>
             </div>
             <h3>Aušra Kazlauskienė</h3>
