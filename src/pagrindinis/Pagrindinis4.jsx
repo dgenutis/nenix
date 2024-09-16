3f3cb7f3b2c71359d4f8cfa478313a94a29a9726
dgenutis <81755948+dgenutis@users.noreply.github.com>
2024-09-16 11:50:15 +0300

    Update Pagrindinis4.jsx

--- src/pagrindinis/Pagrindinis4.jsx
+++ src/pagrindinis/Pagrindinis4.jsx
@@ -7,19 +7,19 @@ const Pagrindinis4 = () => {
       <div className="procesas">
         <h3>Procesas</h3>
         <h2>Procesas, kuris skatina pažangą</h2>
-        <img src="../../public/Procesas.svg" alt="procesas" />
+        <img src="/Procesas.svg" alt="procesas" />
       </div>
       <div className="naujausiDarbai">
         <h2>Naujausi darbai</h2>
         <div className="darbai">
           <div className="darbai-left">
             <div className="darbai-left1">
-              <img src="../../public/Foto1.png" alt="foto1" />
+              <img src="/Foto1.png" alt="foto1" />
               <h3>Tinklalapių vartotojo sąsajos dizainas</h3>
               <p>Kūrybinis vartotojo sąsajos dizainas</p>
             </div>
             <div className="darbai-left2">
-              <img src="../../public/Foto2.png" alt="foto2" />
+              <img src="/Foto2.png" alt="foto2" />
               <h3>Vartotojo sąsajos dizainas</h3>
               <p>Kūrybinis logotipo atnaujinimas</p>
             </div>
@@ -27,20 +27,20 @@ const Pagrindinis4 = () => {
           <div className="darbai-right">
             <button>
               {" "}
-              <img src="../../public/rodykleBalta.svg" alt="" />
+              <img src="/rodykleBalta.svg" alt="" />
               Išbandykite nemokamai
             </button>
             <div className="darbai-right1">
               <NavLink to="/vidinis">
                 {" "}
-                <img src="../../public/Foto3.png" alt="foto3" />
+                <img src="/Foto3.png" alt="foto3" />
               </NavLink>
 
               <h3>Kurti skaitmeninę strategiją</h3>
               <p>Socialinių tinklų Marketingas</p>
             </div>
             <div className="darbai-right2">
-              <img src="../../public/Foto4.png" alt="foto4" />
+              <img src="/Foto4.png" alt="foto4" />
               <h3>Reklaminių banerių kūrimas</h3>
               <p>Grafinis dizainas</p>
             </div>
